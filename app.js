3f919878681c3adc9583b7936746edbf47d50a17
Bishwash Kumar Sah <bishwash.sah2121@gmail.com>
2023-12-07 20:27:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
